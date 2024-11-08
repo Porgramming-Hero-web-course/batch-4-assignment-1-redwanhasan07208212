c45f8fbb245422df8898c1494f5ffc265ab4192b
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2024-11-08 09:20:37 +0600

    change the problem no 3

--- Problem/src/problem-3.ts
+++ Problem/src/problem-3.ts
@@ -1,15 +1,13 @@
 {
   function countWordOccurrences(sentence: string, word: string): number {
     const wordsLowerCaser = sentence.toLowerCase();
+    const wordLower = word.toLowerCase();
     const words = wordsLowerCaser.split(" ");
     // console.log(words);
-    return words.filter((w) => w === word).length;
+    return words.filter((w) => w === wordLower).length;
   }
 
   console.log(
-    countWordOccurrences(
-      "TypeScript is great. I love TypeScript",
-      "typescript"
-    )
+    countWordOccurrences("TypeScript is great. I love TypeScript", "Typescript")
   );
 }
